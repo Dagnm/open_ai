3e891722fc2681e923f1e25e82c7e79cfd64af93
Dagnachew M <dagnachew16@gmail.com>
2023-03-13 02:15:48 +0300

    second  commit

--- client/script.js
+++ client/script.js
@@ -67,7 +67,7 @@ const handleSubmmit = async (e) => {
 
   // fetch data from server
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("http://localhost:5000/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
