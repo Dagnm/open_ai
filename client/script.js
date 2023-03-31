886a8f607590acdafee3742ed74006492b1d7b31
Dagnachew M <dagnachew16@gmail.com>
2023-03-31 12:38:22 +0300

    change

--- client/script.js
+++ client/script.js
@@ -69,7 +69,7 @@ const handleSubmit = async (e) => {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${OPENAI_API_KEY}`,
+      // Authorization: `Bearer ${OPENAI_API_KEY}`,
     },
     body: JSON.stringify({
       prompt: data.get("prompt"),
